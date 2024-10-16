0d021f684e511c8e1b2ea87f22f8e823d9277c98
Mathias Vapper. <mathias.vapper@tktk.ee>
2024-10-16 23:42:50 +0300

    create Funtcions

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are u coming or leaving? (in/out):");
+string userchoice = Console.ReadLine();
+
+if(userchoice == "in")
+{
+    PrintHello();
+}
+else
+{
+PrintGoodBye();
+}
+
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
---
+++ test 7/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("a = Teisenda Celsius Fahrenheitiks, b = teisenda Fahrenheit Celsiuseks.");
+
+string valik = Console.ReadLine();
+
+if (valik == "a")
+{
+    Console.Write("Sisesta temperatuur Celsiuses: ");
+    double celsius =Int32.Parse(Console.ReadLine());
+    ConvertToFahrenheit(celsius);
+}
+else if (valik == "b")
+{
+    Console.Write("Sisesta temperatuur Fahrenheitis: ");
+    double fahrenheit =Int32.Parse(Console.ReadLine());
+    ConvertToCelsius(fahrenheit);
+}
+else
+{
+    Console.WriteLine("Proovi uuesti. Sisesta a või b.");
+}
+    
+
+    static void ConvertToCelsius(double fahrenheit)
+{
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    Console.WriteLine($"{fahrenheit} kraadi Fahrenheiti = {celsius} kraadi Celsiust.");
+}
+
+static void ConvertToFahrenheit(double celsius)
+{
+    double fahrenheit = (celsius * 9) / 5 + 32;
+    Console.WriteLine($"{celsius} kraadi Celsiust = {fahrenheit} kraadi Fahrenheiti.");
+}
